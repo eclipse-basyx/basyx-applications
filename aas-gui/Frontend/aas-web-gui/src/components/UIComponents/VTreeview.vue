a4876bdcf8d33d0d803c3347a821c11976390973
Sebastian Eicke <sebastian.eicke@harting.com>
2024-03-16 05:22:32 +0100

    Use displayName ('en', if available) instead of idShort in VTreeView

--- aas-gui/Frontend/aas-web-gui/src/components/UIComponents/VTreeview.vue
+++ aas-gui/Frontend/aas-web-gui/src/components/UIComponents/VTreeview.vue
@@ -3,7 +3,10 @@
         <!-- List Item with a Submodel / SubmodelelementCollection / submodelElement (like Property) -->
         <!-- TODO: Fix weird Ripple effect on isActive change to false -->
         <v-list-item @click="toggleNode()" :style="{ 'padding-left': depth * 22 + 'px' }" density="compact" class="py-0" nav color="primary" :active="item.isActive">
-            <v-list-item-title>{{ item.idShort ? item.idShort : '' }}</v-list-item-title>
+            <v-list-item-title v-if="item.displayName && item.displayName.find(displayName => { return (displayName.language === 'en' && displayName.text !== ''); })">
+                {{ item.displayName.find(displayName => { return (displayName.language === 'en' && displayName.text !== ''); }).text }}
+            </v-list-item-title>
+            <v-list-item-title v-else>{{ (item.idShort ? item.idShort : '') }}</v-list-item-title>
             <template v-slot:prepend>
                 <!-- Button to show/hide children -->
                 <v-btn v-if="item.children" size="small" variant="plain" @click.stop="toggleChildren()" :icon="showChildren ? 'mdi-menu-down' : 'mdi-menu-right'" :ripple="false"></v-btn>
